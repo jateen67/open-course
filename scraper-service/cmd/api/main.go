4ba67b00f9172d9e50f6b7bc42cad9356d3ed024
exit <kalsijatin67@gmail.com>
2024-07-20 23:36:26 -0400

    expanded rabbit

--- scraper-service/cmd/api/main.go
+++ scraper-service/cmd/api/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"log"
 	"os"
 	"time"
 
+	event "github.com/jateen67/scraper-service/rabbit"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -18,61 +18,10 @@ func main() {
 	}
 	defer conn.Close()
 
-	ch, err := conn.Channel()
+	err = event.Listen(conn)
 	if err != nil {
-		log.Fatalf("could not open rabbitmq channel: %s", err)
+		log.Fatalf("could not listen to queue: %s", err)
 	}
-	defer ch.Close()
-
-	q, err := ch.QueueDeclare(
-		"db_change_queue", // name
-		true,              // durable
-		false,             // delete when unused
-		false,             // exclusive
-		false,             // no-wait
-		nil,               // arguments
-	)
-	if err != nil {
-		log.Fatalf("could not declare queue: %s", err)
-	}
-
-	err = ch.Qos(
-		1,     // prefetch count
-		0,     // prefetch size
-		false, // global
-	)
-	if err != nil {
-		log.Fatalf("could not set QoS: %s", err)
-	}
-
-	msgs, err := ch.Consume(
-		q.Name, // queue
-		"",     // consumer
-		false,  // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
-	)
-	if err != nil {
-		log.Fatalf("could not register consumer: %s", err)
-	}
-
-	var forever chan struct{}
-
-	go func() {
-		for d := range msgs {
-			log.Printf("Received a message: %s", d.Body)
-			dotCount := bytes.Count(d.Body, []byte("."))
-			t := time.Duration(dotCount)
-			time.Sleep(t * time.Second)
-			log.Printf("Done")
-			d.Ack(false)
-		}
-	}()
-
-	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
-	<-forever
 }
 
 func connectToRabbitMQ() (*amqp.Connection, error) {
