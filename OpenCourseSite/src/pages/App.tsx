6ef9cde35921a77f670c7256da92a989517d3526
Rei Kong <reiikongs@gmail.com>
2024-07-28 00:23:20 +0800

    added select menu component

--- OpenCourseSite/src/pages/App.tsx
+++ OpenCourseSite/src/pages/App.tsx
@@ -1,8 +1,18 @@
+import { useEffect, useState } from "react";
 import Test from "../components/test";
+import Nav from "../components/Nav"
+import Colors, { applyTheme } from '../styles/ColorSystem';
 
 export default function App() {
+  const [currentTheme, setCurrentTheme] = useState<keyof typeof Colors>("blue");
+
+  useEffect(() => {
+    applyTheme(Colors[currentTheme]);
+  }, [currentTheme]);
+
   return (
     <>
+      <Nav setCurrentTheme={setCurrentTheme} />
       <Test />
     </>
   );
