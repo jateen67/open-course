4eef43334b2c2f76e61b3fae10189a7fcf5e505a
exit <kalsijatin67@gmail.com>
2024-07-27 10:15:18 -0400

    small change

--- order-service/cmd/api/routes.go
+++ order-service/cmd/api/routes.go
@@ -5,8 +5,8 @@ func (s *server) routes() {
 	s.Router.Get("/scrapercourses", s.getAllScraperCourses)
 	s.Router.Get("/orders", s.getOrders)
 	s.Router.Get("/orderbyid/{id}", s.getOrderByID)
-	s.Router.Get("/orderbyemail/{email}", s.getOrdersByEmail)
-	s.Router.Get("/orderbycourseid/{courseId}", s.getOrdersByCourseID)
+	s.Router.Get("/ordersbyemail/{email}", s.getOrdersByEmail)
+	s.Router.Get("/ordersbycourseid/{courseId}", s.getOrdersByCourseID)
 	s.Router.Post("/orders", s.createOrder)
 	s.Router.Put("/orders", s.editOrder)
 }
