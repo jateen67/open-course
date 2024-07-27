ada3cde1a31d53c9f651e66e1adf5f1bda34bcda
exit <kalsijatin67@gmail.com>
2024-07-27 13:09:36 -0400

    Revert "added select menu component"
    This reverts commit 6ef9cde35921a77f670c7256da92a989517d3526.

--- OpenCourseSite/src/pages/App.tsx
+++ OpenCourseSite/src/pages/App.tsx
@@ -1,18 +1,8 @@
-import { useEffect, useState } from "react";
 import Test from "../components/test";
-import Nav from "../components/Nav"
-import Colors, { applyTheme } from '../styles/ColorSystem';
 
 export default function App() {
-  const [currentTheme, setCurrentTheme] = useState<keyof typeof Colors>("blue");
-
-  useEffect(() => {
-    applyTheme(Colors[currentTheme]);
-  }, [currentTheme]);
-
   return (
     <>
-      <Nav setCurrentTheme={setCurrentTheme} />
       <Test />
     </>
   );
