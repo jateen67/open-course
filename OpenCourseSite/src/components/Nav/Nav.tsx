799278f526b2c3a3a9d464c20ed57e048f637d08
Rei Kong <reiikongs@gmail.com>
2024-07-28 23:18:03 +0800

    nav styling

---
+++ OpenCourseSite/src/components/Nav/Nav.tsx
@@ -0,0 +1,21 @@
+import { SelectMenu } from "../SelectMenu/SelectMenu"
+import NavStyles from "./Nav.module.css"
+import Colors from "../../styles/ColorSystem"
+
+interface NavProps {
+    setCurrentTheme: (theme: keyof typeof Colors) => void;
+}
+
+export default function Nav(props: NavProps) {
+    return (
+        <div className={NavStyles.Content}>
+            <div className={NavStyles.ContentLeft}>
+                <p className={NavStyles.Text}>OpenCourse</p>
+                <SelectMenu setCurrentTheme={props.setCurrentTheme} />
+            </div>
+            <div>
+                <p className={NavStyles.Text}>About</p>
+            </div>
+        </div>
+    )
+}
