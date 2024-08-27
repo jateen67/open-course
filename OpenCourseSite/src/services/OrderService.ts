5652c76182fd159a93935ba736e6c1d9cdd4b609
exit <kalsijatin67@gmail.com>
2024-08-26 21:23:15 -0400

    removed unused routes

--- OpenCourseSite/src/services/OrderService.ts
+++ OpenCourseSite/src/services/OrderService.ts
@@ -1,46 +1,8 @@
-import {
-  Fetch,
-  getParams,
-  GlobalEnvironment,
-  postParams,
-  putParams,
-} from "helpers";
+import { Fetch, GlobalEnvironment, postParams } from "helpers";
 import { Order } from "models";
 import { Observable } from "rxjs";
 
 export class OrderService {
-  static GetAll(): Observable<Order[]> {
-    return Fetch<Order[]>(`${GlobalEnvironment.GetUrlApi()}/orders`, getParams);
-  }
-
-  static GetOrderById(id: number): Observable<Order> {
-    return Fetch<Order>(
-      `${GlobalEnvironment.GetUrlApi()}/orderbyid/${id}`,
-      getParams
-    );
-  }
-
-  static GetOrderByEmail(email: string): Observable<Order[]> {
-    return Fetch<Order[]>(
-      `${GlobalEnvironment.GetUrlApi()}/ordersbyemail/${email}`,
-      getParams
-    );
-  }
-
-  static GetOrderByCourseId(courseId: number): Observable<Order[]> {
-    return Fetch<Order[]>(
-      `${GlobalEnvironment.GetUrlApi()}/ordersbycourseid/${courseId}`,
-      getParams
-    );
-  }
-
-  static UpdateOrder(order: Order): Observable<Order> {
-    return Fetch<Order>(
-      `${GlobalEnvironment.GetUrlApi()}/orders`,
-      putParams(JSON.stringify(order))
-    );
-  }
-
   static CreateOrder(order: Order): Observable<Order> {
     return Fetch<Order>(
       `${GlobalEnvironment.GetUrlApi()}/orders`,
