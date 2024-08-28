6b8f59b29710932d83ffe30934ab539b386dfd31
exit <kalsijatin67@gmail.com>
2024-08-27 20:10:19 -0400

    about me progress

--- OpenCourseSite/src/pages/AboutPage/index.tsx
+++ OpenCourseSite/src/pages/AboutPage/index.tsx
@@ -14,20 +14,39 @@ export const AboutPage: React.FC = () => {
     <>
       <Nav />
       <div className={MainContentStyles.AboutContent}>
-        <h3>About ConUAlerts</h3>
+        <h3>How does ConUAlerts work?</h3>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et
-          nisi eget ligula scelerisque euismod sed ac metus. Praesent vel nisi
-          vel erat luctus congue. Vestibulum euismod mollis est at hendrerit.
-          Curabitur euismod mauris risus, non faucibus eros accumsan vel. In
-          interdum augue ex, at bibendum urna finibus in. Praesent tellus lacus,
-          hendrerit vitae luctus non, facilisis a risus. Proin consectetur
-          ligula et tortor tempor vulputate. Sed posuere vestibulum ex, a
-          venenatis purus dapibus id. Mauris aliquet enim vel ornare
-          scelerisque. Nam convallis est justo, eu porttitor arcu scelerisque
-          posuere. Ut ac rhoncus eros. Morbi ac nulla tempor, suscipit felis
-          vitae, euismod libero.
+          We constantly grab information from VSB Concordia to detect if a
+          course seat or waitlist position has opened. If it has, you receive a
+          text message.
         </p>
+        <h3>
+          I made an order for a course last semester, but that order has
+          expired. Why?
+        </h3>
+        <p>
+          If you signed up for course notifications for a certain semester, like
+          Fall 2024, you must sign up for notifications for that same course in
+          the following semester.
+        </p>
+        <h3>
+          I received an alert that a course has opened, but there weren't any
+          when I checked. Why?
+        </h3>
+        <p>
+          Someone sniped the spot before you could :( No worries, simply text
+          "ORDERS" to view the ID of the course, then "START &lt;course
+          number&gt;" to reinstate your order for that course!
+        </p>
+        <h3>How can I contact you?</h3>
+        <p>
+          For more details about our service, or any general information
+          regarding ConUAlerts, feel free to reach out to us at{" "}
+          <a href="mailto:help@conualerts.com">help@conualerts.com</a>. We are
+          here to provide support and answer any questions you may have.
+        </p>
+        <br></br>
+        <p>Made with &#9829; by Rei Kong, Danny Mousa, Jatin Kalsi</p>
       </div>
     </>
   );
